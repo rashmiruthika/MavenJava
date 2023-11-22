cf7f2d3e36f9209ee28dbdb055fd0d7601ec5a77
rashmiruthika <21bd1a05dccseg@gmail.com>
2023-11-22 10:15:05 +0530

    Maven java push

---
+++ src/test/java/Team_SE/Sample/AppTest.java
@@ -0,0 +1,20 @@
+package Team_SE.Sample;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
